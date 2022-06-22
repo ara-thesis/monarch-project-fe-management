794d0e2f5e5a8dd1d1c2722ca90d13fd3f16addd
Arsyi-sul10 <sulthoniarsyii@gmail.com>
2022-06-23 01:04:34 +0700

    fix bugs news

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -22,7 +22,7 @@ const Form = ({addMessage}) => {
                 date
                 })) 
                 setTitle("")
-                setStatus("Status = ")
+                setStatus("")
                 setMessage("")
                 setError(false);
             }
@@ -30,20 +30,41 @@ const Form = ({addMessage}) => {
 
     return (
         <div className="form">
-            <h1 style={{textAlign:"center"}}>New</h1>
+            <h1 style={{textAlign:"left", fontWeight:"bold", marginBottom:"20px" }}>Add News</h1>
             <form onSubmit={handleSubmit}>
+
                 <input 
                     type="text" 
                     placeholder="Title" 
                     onChange={(e) => setTitle(e.target.value)}
                     value={title}
+                    style={{
+                        width: "40%",
+                        paddingLeft: "8px",
+                        paddingTop: "6px",
+                        paddingBottom: "6px",
+                        paddingRight: "6px",
+                        marginBottom:"40px",
+                    }}
                 />
-                                <input 
+                
+                <input 
                     type="text" 
                     placeholder="Status" 
                     onChange={(e) => setStatus(e.target.value)}
                     value={status}
+                    style={{
+                        width: "30%",
+                        paddingLeft: "8px",
+                        paddingTop: "6px",
+                        paddingBottom: "6px",
+                        paddingRight: "6px",
+                        marginLeft:"20px",
+                    }}
                 /> 
+
+
+                
                 
                 <textarea 
                     style={{border: error ? "1px solid red" : ".5px solid rgba(128, 128, 128, 0.555)"}}
@@ -51,9 +72,9 @@ const Form = ({addMessage}) => {
                     rows="18"
                     onChange={(e) => setMessage(e.target.value)}
                     value={message}
-                    placeholder="Ecrire votre message"
+                    placeholder="Tulis berita anda"
                 ></textarea>
-                {error && <div style={{textAlign:"center", color:"red"}}>Le nombre de caractere de votre message doit superieur a 140 !</div>}
+                {error && <div style={{textAlign:"center", color:"red"}}>Jumlah karakter dalam berita anda harus lebih dari 140!</div>}
                 
                 <div className="submit">
                     <input type="submit" value="Send"/>  
