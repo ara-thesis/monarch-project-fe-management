ba61d3e2d9520356a908746d31d715e63169a660
Arsyi-sul10 <sulthoniarsyii@gmail.com>
2022-06-23 00:18:59 +0700

    Revert "Completeing news add"
    This reverts commit 10d7d32d4fc36a10317303b9c085f52bf8f367ba.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,24 +2,10 @@ import React, { useState } from 'react'
 
 const Form = ({addMessage}) => {
 
-    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
     const [message, setMessage] = useState("");
-    const [status,  setStatus] = useState("");
+    const [date, setDate] = useState(Date.now());
     const [error, setError] = useState(false);
-
-    const [selectedImage, setSelectedImage] = useState();
-
-  // This function will be triggered when the file field change
-  const imageChange = (e) => {
-    if (e.target.files && e.target.files.length > 0) {
-      setSelectedImage(e.target.files[0]);
-    }
-  };
-
-  // This function will be triggered when the "Remove This Image" button is clicked
-  const removeSelectedImage = () => {
-    setSelectedImage();
-  };
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -30,68 +16,34 @@ const Form = ({addMessage}) => {
                 addMessage({
                 id: new Date().getTime(),
                 author,
-                title,
                 message,
-                status,
+                date
                 })) 
-                setTitle("")
+                setAuthor("")
                 setMessage("")
-                setStatus("")
                 setError(false);
-
             }
         }
 
     return (
         <div className="form">
-            <h1 style={{textAlign:"center"}}>Add <News></News></h1>
+            <h1 style={{textAlign:"center"}}>New</h1>
             <form onSubmit={handleSubmit}>
                 <input 
                     type="text" 
-                    placeholder="Title" 
-                    onChange={(e) => setTitle(e.target.value)}
-                    value={title}
+                    placeholder="Author" 
+                    onChange={(e) => setAuthor(e.target.value)}
+                    value={author}
                 />
                 
-                <input 
-                    type="text" 
-                    placeholder="Status" 
-                    onChange={(e) => setStatus(e.target.value)}
-                    value={title}
-                />
-
-
                 <textarea 
                     style={{border: error ? "1px solid red" : ".5px solid rgba(128, 128, 128, 0.555)"}}
                     cols="100" 
                     rows="20"
                     onChange={(e) => setMessage(e.target.value)}
                     value={message}
-                    placeholder="About Place"
+                    placeholder="Tulis Berita yang ingin anda"
                 ></textarea>
-
-                <div style={styles.container}>
-                <input
-                accept="image/*"
-                type="file"
-                onChange={imageChange}
-                />
-
-                {selectedImage && (
-                <div style={styles.preview}>
-                <img
-                src={URL.createObjectURL(selectedImage)}
-                style={styles.image}
-                alt="Thumb"
-            />
-            <button onClick={removeSelectedImage} style={styles.delete}>
-              Remove This Image
-            </button>
-          </div>
-        )}
-      </div>
-
-
                 {error && <div style={{textAlign:"center", color:"red"}}>Jumlah karakter dalam pesan Anda harus lebih dari 140 karakter!</div>}
                 
                 <div className="submit">
@@ -101,5 +53,5 @@ const Form = ({addMessage}) => {
             </form>
         </div>
     )
-}
+} 
 export default Form;
