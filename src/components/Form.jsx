80eb94ef2d8080b2181d00d267955e88b33560c0
Arsyi-sul10 <sulthoniarsyii@gmail.com>
2022-06-23 01:35:56 +0700

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,28 +2,69 @@ import React, { useState } from 'react'
 
 const Form = ({addMessage}) => {
 
+    //CSS
+
+    const styles = {
+        container: {
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          paddingTop: 50,
+        },
+        preview: {
+          marginTop: 50,
+          display: "flex",
+          flexDirection: "column",
+        },
+        image: { maxWidth: "100%", maxHeight: 320 },
+        delete: {
+          cursor: "pointer",
+          padding: 15,
+          background: "red",
+          color: "white",
+          border: "none",
+        },
+      };
+
     const [title, setTitle] = useState("");
     const [status,  setStatus] = useState("");
     const [message, setMessage] = useState("");
     const [date, setDate] = useState(Date.now());
     const [error, setError] = useState(false);
+    const [selectedImage, setSelectedImage] = useState();
+
+
+    //function ini dipanggil ketika file akan diganti/di change
+    const imageChange = (e) => {
+        if (e.target.files && e.target.files.length > 0) {
+          setSelectedImage(e.target.files[0]);
+        }
+    };
+    
+    //function ini dipanggil ketika file akan dihapus
+    const removeSelectedImage = () => {
+        setSelectedImage();
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
         if(message.length < 140){
             setError(true);
         }else{
-            (title === "" || status == ""|| message === "") ? alert("Veuillez renseigner tous les champs") : (
+            (title === "" || status == ""|| message === "" || selectedImage == "") ? alert("Veuillez renseigner tous les champs") : (
                 addMessage({
                 id: new Date().getTime(),
                 title,
                 status,
                 message,
+                selectedImage,
                 date
                 })) 
                 setTitle("")
                 setStatus("")
                 setMessage("")
+                setSelectedImage("")
                 setError(false);
             }
         }
@@ -74,11 +115,47 @@ const Form = ({addMessage}) => {
                     value={message}
                     placeholder="Tulis berita anda"
                 ></textarea>
+
+
+        <>
+      <div style={styles.container}>
+        <input
+          accept="image/*"
+          type="file"
+          onChange={imageChange}
+          style={{
+            width: "30%",
+            paddingLeft: "8px",
+            paddingTop: "6px",
+            paddingBottom: "6px",
+            paddingRight: "6px",
+            marginTop:"20px",
+        }}
+        />
+
+        {selectedImage && (
+          <div style={styles.preview}>
+            <img
+              src={URL.createObjectURL(selectedImage)}
+              style={styles.image}
+              alt="Thumb"
+            />
+            <button onClick={removeSelectedImage} style={styles.delete}>
+              Remove This Image
+            </button>
+          </div>
+        )}
+      </div>
+    </>
+
+
                 {error && <div style={{textAlign:"center", color:"red"}}>Jumlah karakter dalam berita anda harus lebih dari 140!</div>}
                 
                 <div className="submit">
                     <input type="submit" value="Send"/>  
                 </div>
+
+                
                 
             </form>
         </div>
