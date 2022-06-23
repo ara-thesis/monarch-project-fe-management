480cafa791c7a8eb9ed985c25842a26eb7fed392
Arsyi-sul10 <sulthoniarsyii@gmail.com>
2022-06-23 18:21:17 +0700

    Revert "Update untuk news"
    This reverts commit 02714cc412e7db081418130a3dbd6d0f13fc8307.

--- src/pages/NewsAdd.js
+++ src/pages/NewsAdd.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
+import Articles from "../components/Articles";
 import Form from '../components/Form';
-import { Link } from "react-router-dom";
-import { Button} from 'react-bootstrap';
 
 const NewsAdd = () =>{
 
@@ -26,16 +25,11 @@ const NewsAdd = () =>{
         <div>
             <Form addMessage={handleContent}/>
 
-            <div style={{ textAlign:'left' ,fontWeight:"bold" }}>
-            <h4>Berita anda telah ditambahkan! </h4>
-
-            <Link to="/News">
-  <Button variant="primary" size="sm" textAlign="right">
-    Klik disini untuk kembali
-  </Button>
-</Link>
-            </div>
-
+            {data
+                .sort((a,b) => b.id - a.id)
+                .map((article) => (
+                <Articles key={article.id} article={article} deleteArticle={handleDelete}/>
+            ))}
 
         </div>
     )
